0c554f4ca2ec0eb002cf387a84301781140087be
Gor B. <92037197+Frogleim@users.noreply.github.com>
2024-04-26 13:52:05 +0400

    deploy added

--- controller.py
+++ controller.py
@@ -1,13 +1,14 @@
 import serial
-import time 
+import time
+
 # Define the necessary constants
 ADDR = 0xf1  # Replace with the address of your device
 RESET_COM = 0x01
-COM = 0x02 #COM_EXTANTION_DISPENSE = $02;   
+COM = 0x02  #COM_EXTANTION_DISPENSE = $02;
 CS = 0x04  # Replace with the correct checksum calculation
 NDECK = 1  # Replace with the actual shelf number
 NDISP = 5  # Replace with the actual spiral number
-VAL = 0x01   # Replace with the value to turn up (1 to 10)
+VAL = 0x01  # Replace with the value to turn up (1 to 10)
 
 COM_PORT = '/dev/ttyACM0'  # Replace with your actual COM port name
 BAUD_RATE = 9600
@@ -30,27 +31,30 @@ def reset_devices():
 
 def send_command(command):
     ser = serial.Serial(COM_PORT, baudrate=BAUD_RATE)
-    ser.timeout = 4.3
+    ser.timeout = 2.3
     ser.write(command)
     #response = ser.read(1)
-    response_all =  ser.readlines()
+    response_all = ser.readlines()
 
     response = ser.read(2)
     ser.close()
     print(f'All response: {response_all}')
     return response
 
+
 def send_command_and_read_all(command):
     ser = serial.Serial(COM_PORT, baudrate=BAUD_RATE)
-    ser.timeout = 4.3
+    ser.timeout = 0.3
     ser.write(command)
     response = ser.readline()
     ser.close()
     return response
 
+
 # Function to calculate the checksum byte for a command
 def calculate_checksum(cmd):
     return sum(cmd) % 256
+
 
 # Function to send the turn up command
 def turn_up_spring():
@@ -61,6 +65,7 @@ def turn_up_spring():
     print(response)
     response = get_state()
     return response
+
 
 def get_good(ndeck=None, ndisp=None):
     checksum = calculate_checksum([ADDR, COM, ndeck, ndisp])
@@ -73,12 +78,12 @@ def get_good(ndeck=None, ndisp=None):
     print('State: ', response)
     return response
 
+
 def get_state():
     checksum = calculate_checksum([ADDR, COM_TRANSLATOR_POLL])
     command = bytes([ADDR, COM_TRANSLATOR_POLL, checksum])
     response = send_command_and_read_all(command)
     return response
-
 
 
 # Main program
--- main.py
+++ main.py
@@ -1,34 +1,34 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 import controller
 import time
+
 
 class Good(BaseModel):
     shelf_number: int
     spiral_number: int
 
 
-def bytes_translator(shelf, spiral):
-    number = 5
-    d = b'0xnumber'
-    shelf_bytes = bytes(f'{shelf:02X}', 'utf-8')
-    index = d.index(b'number')
-    translated_shelf = d[:index] + shelf_bytes + d[index + len(b'number'):]
-    # Translate spiral to bytes
-    spiral_bytes = bytes(f'{spiral:02X}', 'utf-8')
-    index = d.index(b'number')
-    translated_spiral = d[:index] + spiral_bytes + d[index + len(b'number'):]
-    
-    return translated_shelf, translated_spiral
-
 app = FastAPI()
+
+
+@app.middleware("http")
+async def whitelist_ips(request: Request, call_next):
+    client_host = request.client.port
+    # Only allow 0.0.0.0 to access this API
+    if client_host != "5000":
+        return JSONResponse(status_code=403, content={"message": "Access forbidden"})
+    response = await call_next(request)
+    return response
+
 
 @app.post('/get_goods/')
 def goods(data: Good):
     print('-------------------')
-    for i in range(15):
+    for i in range(8):
         response = controller.get_good(ndeck=data.shelf_number, ndisp=data.spiral_number)
-#        time.sleep(18)
+        #        time.sleep(18)
         if b"\x1a" in response:
             return {'Response': response}
         else:
@@ -40,7 +40,7 @@ def turn_up(data: Good):
     print('-------------------')
     for i in range(15):
         response = controller.get_good(ndeck=data.shelf_number, ndisp=data.spiral_number)
-#        time.sleep(18)
+        #        time.sleep(18)
         if b"\x1a" in response:
             return {'Response': response}
         else:
